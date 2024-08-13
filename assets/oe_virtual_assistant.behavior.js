15785f2b8c3b0a40aeee329a5f08d7ce1a79a6f7
Antonio De Marco <antonio@nuvole.org>
2024-08-13 15:19:22 +0200

    D8TSDT-519: Improve behavior.

--- assets/oe_virtual_assistant.behavior.js
+++ assets/oe_virtual_assistant.behavior.js
@@ -1,14 +1,22 @@
 import ReactDOM from "react-dom";
 import Chat from "./virtual-assistant-chat.js";
 
-// js/my-behavior.js
 (function (Drupal) {
-  Drupal.behaviors.myCustomBehavior = {
+  Drupal.behaviors.VirtualAssistantBehavior = {
     attach: function (context, settings) {
+      // @todo: create an empty element and use that one instead.
       const domContainer = document.querySelector('#content');
       const root = ReactDOM.createRoot(domContainer);
       const chat = Chat({
-        url: "http://localhost:5000"
+        // @todo: make this a Drupal config in due time.
+        url: "http://localhost:5000",
+        style: {
+          position: "absolute",
+          width: "600px",
+          height: "800px",
+          top: 0,
+          right: 0
+        }
       });
       root.render(chat);
     }
